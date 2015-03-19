a392f8aab28bce02df9e9b7466aeded674d63a97
campitos <campitos@campitos-20-b448la>
2015-03-19 16:47:39 -0600

    @daton Subiendo estilos con thymeleaf

--- src/com/capitulo5/herencia/TestAreas.java
+++ src/com/capitulo5/herencia/TestAreas.java
@@ -7,9 +7,11 @@ public class TestAreas {
     public static void main(String[] args) {
         // TODO code application logic here
          try{
-       // Area a=new Area(new Cuadrado(-4));
-   //     System.out.println(a.calcular());
-        
+             
+             for(Superficies s: GenerarFiguras.generarSuperficies()){
+                System.out.println( s.calcularArea());  
+             }
+   
         System.out.println("Si se lanza la excepcion nunca veras esta linea");
          }catch(Exception  e){
              System.out.println(e.getMessage());
